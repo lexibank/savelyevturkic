68bb41811a9a2e736c3620f287741bc0bb012151
xrotwang <xrotwang@googlemail.com>
2018-10-11 10:31:16 +0200

    now with root

--- lexibank_savelyevturkic.py
+++ lexibank_savelyevturkic.py
@@ -1,15 +1,22 @@
 # coding=utf-8
 from __future__ import unicode_literals, print_function
 
+import attr
 from clldutils.path import Path
 from clldutils.misc import slug
 from csvw.dsv import reader
-from pylexibank.dataset import Language, Lexeme, Concept, Dataset as BaseDataset
+from pylexibank.dataset import Cognate, Dataset as BaseDataset
+
+
+@attr.s
+class TurkicCognate(Cognate):
+    root = attr.ib(default=None)
 
 
 class Dataset(BaseDataset):
     id = 'savelyevturkic'
     dir = Path(__file__).parent
+    cognate_class = TurkicCognate
 
     def cmd_download(self, **kw):
         self.raw.xls2csv('Savelyev_Turkic.xls')
@@ -39,6 +46,7 @@ class Dataset(BaseDataset):
                             index2lang[j] = slug(col)
                             ds.add_language(ID=index2lang[j], Name=col)
                 elif i > 1:
+                    has_cognates = False
                     if row[0]:
                         meaning = '{0}_l{1}'.format(slug(row[0]), i + 1)
                         root_index = 1
@@ -51,4 +59,8 @@ class Dataset(BaseDataset):
                             for lex in ds.add_lexemes(Value=col, Language_ID=index2lang[j - 1], Parameter_ID=meaning):
                                 ds.add_cognate(
                                     lexeme=lex,
-                                    Cognateset_ID='%s-%s' % (meaning, root_index))
+                                    root=root,
+                                    Cognateset_ID='%s-%s-l%s' % (meaning, root_index, i + 1))
+                                has_cognates = True
+                    if not has_cognates:
+                        print(i + 1)

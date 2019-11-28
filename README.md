7223012ec536814c5fb02f68623b35dae6420fdc
lingulist <mattis.list@lingpy.org>
2019-11-28 12:37:35 +0100

    finalizing savelyevturkic

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 Cite the source dataset as
 
-> Savelyev, Alexander (2019): Turkic Basic Vocabulary Databse. Jena: Max Planck Institute for the Science of Human History.
+> Savelyev, Alexander and Robbeets, Martine (2019): Turkic Basic Vocabularies. Jena: Max Planck Institute for the Science of Human History.
 
 This dataset is licensed under a https://creativecommons.org/licenses/by/4.0/ license
 
+Available online at https://doi.org/10.5281/zenodo.3555173
+
+
+Conceptlists in Concepticon:
+- [Savelyev-2019-254](https://concepticon.clld.org/contributions/Savelyev-2019-254)
 ## Notes
 
 If you want to replicate the study, you can receive the NEXUS file using the EDICTOR tool (http://edictor.digling.org). In order to this, you should:
@@ -23,8 +28,7 @@ If you want to replicate the study, you can receive the NEXUS file using the EDI
 ## Statistics
 
 
-
-![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Glottolog: 97%](https://img.shields.io/badge/Glottolog-97%25-green.svg "Glottolog: 97%")
 ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
 ![BIPA: 100%](https://img.shields.io/badge/BIPA-100%25-brightgreen.svg "BIPA: 100%")
@@ -32,11 +36,12 @@ If you want to replicate the study, you can receive the NEXUS file using the EDI
 
 - **Varieties:** 32
 - **Concepts:** 254
-- **Lexemes:** 8,359
+- **Lexemes:** 8,360
+- **Sources:** 60
 - **Synonymy:** 1.10
-- **Cognacy:** 8,359 cognates in 905 cognate sets (245 singletons)
+- **Cognacy:** 8,360 cognates in 905 cognate sets (245 singletons)
 - **Cognate Diversity:** 0.08
-- **Invalid lexemes:** 0
-- **Tokens:** 33,352
+- **Invalid lexemes:** 1
+- **Tokens:** 33,355
 - **Segments:** 90 (0 BIPA errors, 0 CTLS sound class errors, 90 CLTS modified)
 - **Inventory size (avg):** 37.25
--- lexibank_savelyevturkic.py
+++ lexibank_savelyevturkic.py
@@ -1,66 +1,65 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-
 import attr
-from clldutils.path import Path
+from pathlib import Path
 from clldutils.misc import slug
-from csvw.dsv import reader
-from pylexibank.dataset import Cognate, NonSplittingDataset as BaseDataset
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank import Cognate, Language
 from lingpy import *
-from pylexibank.util import pb
+from pylexibank import progressbar
 
 
 @attr.s
-class TurkicCognate(Cognate):
-    root = attr.ib(default=None)
+class CustomCognate(Cognate):
+    Root = attr.ib(default=None)
+
+@attr.s
+class CustomLanguage(Language):
+    Source = attr.ib(default=None)
 
 
 class Dataset(BaseDataset):
     id = 'savelyevturkic'
     dir = Path(__file__).parent
-    cognate_class = TurkicCognate
+    cognate_class = CustomCognate
+    language_class = CustomLanguage
 
+    def cmd_download(self, args):
+        self.raw_dir.download(
+                "https://zenodo.org/record/3555174/files/turkic_alignment.tsv?download=1",
+                'turkic_alignment.tsv'
+                )
 
-    def cmd_install(self, **kw):
+    def cmd_makecldf(self, args):
 
-        modified = {"ž": "ʒ", "nˈ": "nʲ", "lˈ": "lʲ", "gˈ": "gʲ",
+        args.writer.add_sources()
+        sources = {}
+        for language in self.languages:
+            sources[language["ID"]] = [x.lower() for x in
+                    language['Source'].split(',')]
+            args.writer.add_language(**language)
+        segments = {"ž": "ʒ", "nˈ": "nʲ", "lˈ": "lʲ", "gˈ": "gʲ",
                 "rˈ": "rʲ", "pˈ": "pʲ", "s-": "s", "š": "ʃ", "βˈ": "βʲ",
                 "sˈ": "sʲ", "tʃ": "tɕ", "ʦ": "ts"}
+        concepts = args.writer.add_concepts(
+                id_factory=lambda x: x.id.split('-')[-1]+"_"+slug(x.english),
+                lookup_factory='Name')
 
-
-        with self.cldf as ds:
-            l2s = {}
-            for l in self.languages:
-                ds.add_language(ID=l['ID'], Glottocode=l['Glottocode'], 
-                        Name=l['Name'])
-                l2s[l['ID']] = [x.lower() for x in l['Source'].split(',')]
-            
-            ds.add_sources(*self.raw.read_bib())
-
-            wl = Wordlist(self.raw.posix('turkic_alignment.tsv'))
-            for concept in self.concepts:
-                ds.add_concept(
-                        ID=slug(concept['ENGLISH']),
-                        Name=concept['ENGLISH'],
-                        Concepticon_ID=concept['CONCEPTICON_ID']
-                        )
-
-            for idx in pb(wl):
-                for lex in ds.add_lexemes(
-                        Language_ID=wl[idx, 'doculect'],
-                        Parameter_ID=slug(wl[idx, 'concept']),
-                        Value=wl[idx, 'entry'],
-                        Form=wl[idx, 'form'],
-                        Segments=[modified.get(x, x) for x in wl[idx,
-                            'tokens']],
-                        Source=l2s[wl[idx, 'doculect']]
-                        ):
-                    ds.add_cognate(
-                            lexeme=lex,
-                            Cognateset_ID=wl[idx, 'cogid'],
-                            Alignment=[modified.get(x, x) for x in wl[idx,
-                                'alignment']]
-                            )
+        wl = Wordlist(self.raw_dir.joinpath('turkic_alignment.tsv').as_posix())
+        for idx in progressbar(wl):
+            lex = args.writer.add_form_with_segments(
+                    Language_ID=wl[idx, 'doculect'],
+                    Parameter_ID=concepts[wl[idx, 'concept']],
+                    Value=wl[idx, 'entry'] or 'NAN',
+                    Form=wl[idx, 'form'] or 'NAN',
+                    Segments=[segments.get(x, x) for x in wl[idx,
+                        'tokens']],
+                    Source=sources[wl[idx, 'doculect']] or ['']
+                    )
+            args.writer.add_cognate(
+                    lexeme=lex,
+                    Cognateset_ID=wl[idx, 'cogid'],
+                    Alignment=[segments.get(x, x) for x in wl[idx, 'alignment']],
+                    Root=wl[idx, 'root']
+                    )
 
 
 

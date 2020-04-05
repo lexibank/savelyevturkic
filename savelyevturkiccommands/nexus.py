7daabaabb0584c19ee5da7aaec5d1777f374e56e
LinguList <mattis.list@posteo.de>
2020-04-05 16:00:19 +0200

    update

--- savelyevturkiccommands/nexus.py
+++ savelyevturkiccommands/nexus.py
@@ -7,6 +7,7 @@ from lingpy import *
 from lingpy.sequence.sound_classes import token2class
 from lexibank_savelyevturkic import Dataset
 from pylexibank.cli_util import add_dataset_spec
+from lingpy.algorithm import misc
 
 def register(parser):
     parser.add_argument(
@@ -60,27 +61,24 @@ def run(args):
                 classes.add(token2class(c, 'asjp'))
     
     args.log.info('[i] found {0} sounds in the data'.format(len(classes)))
-    sounds = sorted(classes)
-    #new_sounds = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    sounds = sorted(classes) + ['-']
+    new_sounds = '0123456789ABCDEFGHKLMNPQRSTUVWXYZabcdefghklmnpqrstuvwxyz'
+    sound_converter = dict(zip(sounds, new_sounds))
     
-    print(len(cop.clusters))
     for (_, k), vals in cop.clusters.items():
         skip = False
         if len(vals) >= args.threshold and not skip:
             matrix += [[token2class(x, 'asjp') if x not in '-Ø' else '?' if x == 'Ø' else '-' for x in k]]
-    print(len(matrix))
-    input()
+
     
 
     text = '{0} {1}\n'.format(len(matrix[0]), len(matrix))
     for i in range(len(matrix[0])):
         text += '{0:30}'.format(cop.cols[i])+'   '
         for j in range(len(matrix)):
-            text += matrix[j][i]
-            #if matrix[j][i] not in '-?':
-            #    text += [sounds.index(matrix[j][i])]
-            #else:
-            #    text += matrix[j][i]
+            text += sound_converter.get(
+                    matrix[j][i],
+                    matrix[j][i])
         text += '\n'
     
     with open('analysis/{0}-{1}-{2}.phy'.format(args.file[:-4],
@@ -89,5 +87,12 @@ def run(args):
         f.write(text)
     args.log.info("[i] the matrix is {0} and {1}".format(
         len(matrix[0]), len(matrix)))
+    with open('analysis/{0}-{1}-{2}.csv'.format(args.file[:-4],
+        args.threshold,
+        args.no_gaps), 'w') as f:
+        matrix = misc.transpose(matrix)
+        for i, tax in enumerate(cop.cols):
+            f.write(tax+','+','.join(matrix[i])+'\n')
+
 
 

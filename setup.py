86dcd3934450ee8bcbd36b576ac035d839696313
chrzyki <christoph@functor-argument.net>
2021-04-22 16:40:16 +0200

    Update scaffolding

--- setup.py
+++ setup.py
@@ -2,25 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json', encoding='utf-8') as fp:
+with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_savelyevturkic',
+    name="lexibank_savelyevturkic",
     version="1.0",
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_savelyevturkic'],
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_savelyevturkic"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'savelyevturkic=lexibank_savelyevturkic:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.1',
-    ]
+    entry_points={"lexibank.dataset": ["savelyevturkic=lexibank_savelyevturkic:Dataset"]},
+    install_requires=["pylexibank>=3.0"],
 )
